8d0ed2a8297074fbc138c578e26c18f206582e7e
Antonio <toony1908@gmail.com>
2015-12-17 00:11:32 -0600

    vistas de los formularios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'inicio/index'
 
-   devise_for :users, :controllers => { registrations: 'users/registrations'}
+   devise_for :users, :controllers => { registrations: 'users/registrations',
+                                        sessions: 'users/sessions'}
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
